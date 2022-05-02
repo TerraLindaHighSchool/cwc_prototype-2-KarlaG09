5dd15084d15142b5a2de328eead4e69739929b2a
KarlaG09 <kg02793@srcs.org>
2022-05-02 15:24:03 -0700

    Finished Unit 2 - Basic Gameplay

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -13,7 +13,7 @@ public class DetectCollisions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if
+
     }
 
     void OnTriggerEnter(Collider other)
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,11 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
-    public GameObject projectilePrefab; 
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
